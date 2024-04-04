998a236945a29df3a321471442aa155baebbf69e
ipfreely-uk <ipfreely.uk@protom.me>
2024-04-04 01:25:43 +0100

    Example

--- ip/subnet/masks_example_test.go
+++ ip/subnet/masks_example_test.go
@@ -1,6 +1,8 @@
 package subnet_test
 
 import (
+	"fmt"
+
 	"github.com/ipfreely-uk/go/ip"
 	"github.com/ipfreely-uk/go/ip/subnet"
 )
@@ -12,4 +14,19 @@ func Example_subnet_Mask() {
 	println("First: %s", network.String())
 	println("Last: %s", mask.Not().Or(network).String())
 	println("Mask: %s", mask.String())
+}
+
+func Example_subnet_AddressCount() {
+	family := ip.V4()
+	for mask := 0; mask <= family.Width(); mask++ {
+		count := subnet.AddressCount(family, mask)
+		msg := fmt.Sprintf("IPv%d /%d ==\t%s", family.Version(), mask, count.String())
+		println(msg)
+	}
+	// family = ip.V6()
+	// for mask := 0; mask <= family.Width(); mask++ {
+	// 	count := subnet.AddressCount(family, mask)
+	// 	msg := fmt.Sprintf("IPv%d /%d ==\t%s", family.Version(), mask, count.String())
+	// 	println(msg)
+	// }
 }
