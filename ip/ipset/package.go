2f76bd205180cfd5899c14ef2fd82c6759235ee6
ipfreely-uk <ipfreely.uk@protom.me>
2024-07-06 16:12:00 +0100

    Documentation

--- ip/ipset/package.go
+++ ip/ipset/package.go
@@ -1,10 +1,13 @@
 // Network ranges and arbitrary IP address sets.
 //
-// [NewInterval] creates contiguous [Interval] set of addresses.
-// [NewBlock] creates RFC-4632 https://www.rfc-editor.org/rfc/rfc4632 CIDR [Block] set.
-// [NewDiscrete] creates non-contigous [Discrete] set of addresses.
+// [NewDiscrete] creates non-contigous [Discrete] set of addresses from [Interval] sets.
+// [NewInterval] creates contiguous [Interval] set of addresses from lower and upper bounds.
+// [NewBlock] creates a RFC-4632 CIDR [Block] set.
+// [NewSingle] creates a [Block] set from a single address.
 //
 // Use [Discrete.Addresses] to iterate over constituent addresses.
-// Use [Discrete.Intervals] to iterate over constituent [Interval] types.
-// Use [Blocks] to iterate over constituent [Block] types within an [Interval] type.
+// Use [Discrete.Intervals] to iterate over constituent [Interval] sets.
+// Use the [Blocks] function to iterate over constituent [Block] sets within [Interval] sets.
+//
+// Reference: https://www.rfc-editor.org/rfc/rfc4632
 package ipset
