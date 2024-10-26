54a1e597a387024229732039ab21a54726486342
ipfreely-uk <ipfreely.uk@protom.me>
2024-10-26 14:17:49 +0100

    Documentation

--- ip/ipset/package.go
+++ ip/ipset/package.go
@@ -1,13 +1,22 @@
-// Network ranges and arbitrary IP address sets.
-//
-// [NewDiscrete] creates non-contigous [Discrete] set of addresses from [Interval] sets.
-// [NewInterval] creates contiguous [Interval] set of addresses from lower and upper bounds.
-// [NewBlock] creates a RFC-4632 CIDR [Block] set.
-// [NewSingle] creates a [Block] set from a single address.
-//
-// Use [Discrete].Addresses to iterate over constituent addresses.
-// Use [Discrete].Intervals to iterate over constituent [Interval] sets.
-// Use the [Blocks] function to iterate over constituent [Block] sets within [Interval] sets.
-//
-// Reference: https://www.rfc-editor.org/rfc/rfc4632
+/*
+Network ranges and arbitrary IP address sets.
+
+# Set Types
+
+[NewInterval] creates contiguous [Interval] set of addresses from lower and upper bounds.
+[NewBlock] creates a RFC-4632 CIDR [Block] set.
+[NewSingle] creates a [Block] set from a single address.
+[NewDiscrete] creates [Discrete] set of addresses from arbitrary sets.
+Use [NewDiscrete] to create the empty set.
+
+# Iteration
+
+Use [Discrete].Addresses to iterate over constituent addresses.
+Use [Discrete].Intervals to iterate over constituent [Interval] sets.
+Use the [Blocks] function to iterate over constituent [Block] sets within [Interval] sets.
+
+# References
+
+https://www.rfc-editor.org/rfc/rfc4632
+*/
 package ipset
--- ip/package.go
+++ ip/package.go
@@ -1,9 +1,13 @@
-// IP addresses as generic, immutable positive integers.
-// Use for arithmetic and bitwise operations.
-//
-// IP address [Family] types act as factories for [Number] values.
-// Use [V4] or [V6] to obtain a [Family].
-//
-// Use [Family].FromBytes or [FromBytes] to obtain [Number] from byte slice.
-// Use [Parse] or [ParseUnknown] to obtain [Number] from string.
+/*
+IP addresses as generic, immutable positive integers.
+Use for arithmetic and bitwise operations.
+
+# IP Address Types
+
+IP address [Family] types act as factories for the [Number] structs [Addr4] and [Addr6].
+Use [V4] or [V6] to obtain a [Family].
+
+Use [Family].FromBytes or [FromBytes] to obtain [Number] from byte slice.
+Use [Parse] or [ParseUnknown] to obtain [Number] from string.
+*/
 package ip
