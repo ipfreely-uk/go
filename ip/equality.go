e10f1d66aa2cf9949b7d2847ea35d68bac014285
ipfreely-uk <ipfreely.uk@protom.me>
2024-11-14 17:56:05 +0000

    Minor enhancement

--- ip/equality.go
+++ ip/equality.go
@@ -2,5 +2,9 @@ package ip
 
 // Tests equality
 func Eq[A Int[A]](address0, address1 A) (areEqual bool) {
-	return address0.Compare(address1) == 0
+	return eq(address0, address1)
+}
+
+func eq(a, b any) bool {
+	return a == b
 }
--- ip/equality_test.go
+++ ip/equality_test.go
@@ -8,9 +8,25 @@ import (
 )
 
 func TestEq(t *testing.T) {
-	one := ip.V4().FromInt(1)
-	two := ip.V4().FromInt(2)
-	assert.True(t, ip.Eq(one, one))
-	assert.False(t, ip.Eq(one, two))
-	assert.False(t, ip.Eq(two, one))
+	{
+		v4 := ip.V4()
+		one := v4.FromInt(1)
+		two := v4.FromInt(2)
+		assert.True(t, ip.Eq(one, v4.FromInt(1)))
+		assert.True(t, ip.Eq(one, one))
+		assert.False(t, ip.Eq(one, two))
+		assert.False(t, ip.Eq(two, one))
+	}
+	{
+		v6 := ip.V6()
+		one := v6.FromInt(1)
+		complement := one.Not()
+		complement2 := one.Not()
+		assert.True(t, ip.Eq(one, v6.FromInt(1)))
+		assert.True(t, ip.Eq(one, one))
+		assert.True(t, ip.Eq(complement, complement))
+		assert.True(t, ip.Eq(complement, complement2))
+		assert.False(t, ip.Eq(one, complement))
+		assert.False(t, ip.Eq(complement, one))
+	}
 }
