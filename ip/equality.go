a3ad32e6f09873d9107ce003ff0f5ccb94421cbe
ipfreely-uk <ipfreely.uk@protom.me>
2024-12-18 14:28:53 +0000

    Minor enhancement

--- ip/equality.go
+++ ip/equality.go
@@ -1,7 +1,7 @@
 package ip
 
 // Tests equality
-func Eq[A Int[A]](address0, address1 A) (areEqual bool) {
+func Eq(address0, address1 Address) (areEqual bool) {
 	return eq(address0, address1)
 }
 
